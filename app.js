c81086158ffae02d71468018279551c85811ea4f
Ephraim Newton <ephraimnewton007@gmail.com>
2024-06-26 14:55:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,332 @@
+const word1 = prompt(`Rearrange the word telsac?`);
+
+if (word1 === `castle`){
+    alert(`Bravo!, ${word1} is correct`);
+} else if (word1 === `Castle` || word1 === `CASTLE`){
+    alert(`Bravo!, ${word1} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word2 = prompt(`Rearrange the word stnseu?`);
+
+ if (word2 === `sunset`){
+    alert(`Bravo!, ${word2} is correct`);
+} else if (word2 === `Sunset` || word2 === `SUNSET`){
+    alert(`Bravo!, ${word2} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word3 = prompt(`Rearrange the word napelt?`);
+
+if (word3 === `planet`){
+    alert(`Bravo!, ${word3} is correct`);
+} else if (word3 === `Planet` || word3 === `PLANET`){
+    alert(`Bravo!, ${word3} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word4 = prompt(`Rearrange the word artkame?`);
+
+if (word4 === `market`){
+    alert(`Bravo!, ${word4} is correct`);
+} else if (word4 === `Market` || word4 === `MARKET`){
+    alert(`Bravo!, ${word4} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word5 = prompt(`Rearrange the word finder?`);
+
+if (word5 === `friend`){
+    alert(`Bravo!, ${word5} is correct`);
+} else if (word5 === `Friend` || word5 === `FRIEND`){
+    alert(`Bravo!, ${word5} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word6 = prompt(`Rearrange the word nopilhd?`);
+
+if (word6 === `dolphin`){
+    alert(`Bravo!, ${word6} is correct`);
+} else if (word6 === `Dolphin` || word6 === `DOLPHIN`){
+    alert(`Bravo!, ${word6} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word7 = prompt(`Rearrange the word lungje?`);
+
+if (word7 === `jungle`){
+    alert(`Bravo!, ${word7} is correct`);
+} else if (word7 === `Jungle` || word7 === `JUNGLE`){
+    alert(`Bravo!, ${word7} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word8 = prompt(`Rearrange the word cimus?`);
+
+if (word8 === `music`){
+    alert(`Bravo!, ${word8} is correct`);
+} else if (word8 === `Music` || word8 === `MUSIC`){
+    alert(`Bravo!, ${word8} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word9 = prompt(`Rearrange the word tigaur?`);
+
+if (word9 === `guitar`){
+    alert(`Bravo!, ${word9} is correct`);
+} else if (word9 === `Guitar` || word9 === `GUITAR`){
+    alert(`Bravo!, ${word9} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word10 = prompt(`Rearrange the word limes?`);
+
+if (word10 === `smile`){
+    alert(`Bravo!, ${word19} is correct`);
+} else if (word10 === `Smile` || word10 === `SMILE`){
+    alert(`Bravo!, ${word19} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word11 = prompt(`Rearrange the word canoe?`);
+
+if (word11 === `ocean`){
+    alert(`Bravo!, ${word119} is correct`);
+} else if (word11 === `Ocean` || word11 === `OCEAN`){
+    alert(`Bravo!, ${word119} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word12 = prompt(`Rearrange the word balm?`);
+
+if (word12 === `lamb`){
+    alert(`Bravo!, ${word12} is correct`);
+} else if (word12 === `Lamb` || word12 === `LAMB`){
+    alert(`Bravo!, ${word12} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word13 = prompt(`Rearrange the word armed?`);
+
+if (word13 === `dream`){
+    alert(`Bravo!, ${word13} is correct`);
+} else if (word13 === `Dream` || word13 === `DREAM`){
+    alert(`Bravo!, ${word13} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word14 = prompt(`Rearrange the word dabscon?`);
+
+if (word14 === `abscond`){ 
+    alert(`Bravo!, ${word14} is correct`);
+} else if (word14 === `Abscond` || word14 === `ABSCOND`){
+    alert(`Bravo!, ${word14} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word15 = prompt(`Rearrange the word sonnerple?`);
+
+if (word15 === `personnel`){
+    alert(`Bravo!, ${word15} is correct`);
+} else if (word15 === `Personnel` || word15 === `PERSONNEL`){
+    alert(`Bravo!, ${word15} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word16 = prompt(`Rearrange the word sewbite?`);
+
+if (word16 === `website`){
+    alert(`Bravo!, ${word16} is correct`);
+} else if (word16 === `Website` || word16 === `WEBSITE`){
+    alert(`Bravo!, ${word16} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word17 = prompt(`Rearrange the word lagoneau?`);
+
+if (word17 === `analogue`){
+    alert(`Bravo!, ${word17} is correct`);
+} else if (word17 === `Analogue` || word17 === `ANALOGUE`){
+    alert(`Bravo!, ${word17} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word18 = prompt(`Rearrange the word vaetrl?`)
+
+if (word18 === `travel`){
+    alert(`Bravo!, ${word18} is correct`);
+} else if (word18 === `Travel` || word18 === `TRAVEL`){
+    alert(`Bravo!, ${word18} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word19 = prompt(`Rearrange the word tiennac?`);
+
+if (word19 === `ancient`){
+    alert(`Bravo!, ${word19} is correct`);
+} else if (word19 === `Ancient` || word19 === `ANCIENT`){
+    alert(`Bravo!, ${word19} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+ const word20 = prompt(`Rearrange the word owfrel?`);
+
+ if (word20 === `flower`){
+    alert(`Bravo!, ${word20} is correct`);
+} else if (word20 === `Flower` || word20 === `FLOWER`){
+    alert(`Bravo!, ${word20} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+ const word21 = prompt(`Rearrange the word bniowar?`);
+
+ if (word21  === `rainbow`){
+    alert(`Bravo!, ${word21 } is correct`);
+} else if (word21  === `Rainbow` || word21  === `RAINBOW`){
+    alert(`Bravo!, ${word21 } is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word22 = prompt(`Rearrange the word potalt?`);
+
+if (word22 === `laptop`){
+    alert(`Bravo!, ${word22} is correct`);
+} else if (word22 === `Laptop` || word22 === `LAPTOP`){
+    alert(`Bravo!, ${word22} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word23 = prompt(`Rearrange the word meusum?`);
+
+if (word23 === `museum`){
+    alert(`Bravo!, ${word23} is correct`);
+} else if (word23 === `Museum` || word23 === `MUSEUM`){
+    alert(`Bravo!, ${word23} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word24 = prompt(`Rearrange the word teetmimoc?`);
+
+if (word24 === `committee`){
+    alert(`Bravo!, ${word24} is correct`);
+} else if (word24 === `COMMITTEE` || word24 === `Committee`){
+    alert(`Bravo!, ${word24} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word25 = prompt(`Rearrange the word strofe?`);
+
+if (word25 === `forest`){
+    alert(`Bravo!, ${word25} is correct`);
+} else if (word25 === `Forest` || word25 === `FOREST`){
+    alert(`Bravo!, ${word25} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word26 = prompt(`Rearrange the word litpaac?`);
+
+if (word26 === `capital`){
+    alert(`Bravo!, ${word26} is correct`);
+} else if (word26 === `Capital` || word26 === `CAPITAL`){
+    alert(`Bravo!, ${word26} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word27 = prompt(`Rearrange the word words?`);
+
+if (word27 === `sword`){
+    alert(`Bravo!, ${word27} is correct`);
+} else if (word27 === `Sword` || word27 === `SWORD`){
+    alert(`Bravo!, ${word27} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word28 = prompt(`Rearrange the word zuzlep?`);
+
+if (word28 === `puzzle`){
+    alert(`Bravo!, ${word28} is correct`);
+} else if (word28 === `Puzzle` || word28 === `PUZZLE`){
+    alert(`Bravo!, ${word28} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word29 = prompt(`Rearrange the word roadkeyb?`);
+
+if (word29 === `keyboard`){
+    alert(`Bravo!, ${word29} is correct`);
+} else if (word29 === `Keyboard` || word29 === `KEYBOARD`){
+    alert(`Bravo!, ${word29} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+const word30 = prompt(`Rearrange the word sewnertr?`);
+
+if (word30 === `western`){
+    alert(`Bravo!, ${word30} is correct`);
+} else if (word30 === `Western` || word30 === `WESTERN`){
+    alert(`Bravo!, ${word30} is correct`);
+}else{
+    alert(`Oops, You are wrong`);
+}
+
+
+
+document.write(`Here is your result and the correct answers, for No1 you entered ${word1}, The answer is "CASTLE". 
+for No2 you entered ${word2}, and the answer is "SUSNSET". 
+for No3 you entered ${word3}, and the answer is "PLANET". 
+for No4 you entered ${word4}, and the answer is "MARKET". 
+for No5 you entered ${word5}, and the answer is "FRIEND". 
+for No6 you entered ${word6}, and the answer is "DOLPHIN". 
+for No7 you entered ${word7}, and the answer is "JUNGLE". 
+for No8 you entered ${word8}, and the answer is "MUSIC". 
+for No9 you entered ${word9}, and the answer is "GUITAR". 
+for No10 you entered ${word10}, and the answer is "SMILE". 
+for No11 you entered ${word11}, and the answer is "OCEAN". 
+for No12 you entered ${word12}, and the answer is "LAMB". 
+for No13 you entered ${word13}, and the answer is "DREAM". 
+for No14 you entered ${word14}, and the answer is "ABSCOND". 
+for No15 you entered ${word15}, and the correct answer is "PERSONNEL". 
+for No16 you entered ${word16}, and the answer is "WEBSITE". 
+for No17 you entered ${word17}, and the answer is "ANALOGUE". 
+for No18 you entered ${word18}, and the answer is "TRAVEL". 
+for No19 you entered ${word19}, and the answer is "ANCIENT". 
+for No20 you entered ${word20}, and the answer is "FLOWER". 
+for No21 you entered ${word21}, and the answer is "RAINBOW". 
+for No22 you entered ${word22}, and the answer is "LAPTOP". 
+for No23 you entered ${word23}, and the answer is "MUSEUM". 
+for No24 you entered ${word24}, and the answer is "COMMITTEE". 
+for No25 you entered ${word25}, and the answer is "FOREST". 
+for No26 you entered ${word26}, and the answer is "CAPITAL". 
+for No27 you entered ${word27}, and the answer is "SWORD". 
+for No28 you entered ${word28}, and the answer is "PUZZLE". 
+for No29 you entered ${word29}, and the answer is "KEYBOARD". 
+for No30 you entered ${word30}, and the answer is "WESTERN".`);
